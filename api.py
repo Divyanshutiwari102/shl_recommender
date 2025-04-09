c03721a950bc183a524807ec8627a745862b87de
Divyanshutiwari102 <143499516+Divyanshutiwari102@users.noreply.github.com>
2025-04-09 21:13:15 +0530

    api.py

--- api.py
+++ api.py
@@ -23,3 +23,8 @@ def recommend():
 
 if __name__ == '__main__':
     app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render will provide the PORT env variable
+    app.run(host='0.0.0.0', port=port)
